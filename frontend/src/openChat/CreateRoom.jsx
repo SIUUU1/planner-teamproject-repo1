e8dcc9c8075ca7a7d8864212985738ec7b3656a3
Sieun Lim <lim2024sieun@gmail.com>
2024-08-30 22:31:59 +0900

    [chore] Update chat css

--- frontend/src/openChat/CreateRoom.jsx
+++ frontend/src/openChat/CreateRoom.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import useSendPost from '../util/useSendPost';
-
+import './CreateRoom.css';
 const CreateRoom = () => {
   const [roomName, setRoomName] = useState('');
   const [roomInfo, setRoomInfo] = useState('');
@@ -16,27 +16,28 @@ const CreateRoom = () => {
     await postRequest({ room_name: roomName,  room_info:roomInfo });
     setRoomInfo('');
     setRoomName('');
+    alert('정상적으로 방이 생성되었습니다!');
   };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
+    <div className='createRoom'>
       <h1>채팅방 생성</h1>
-      <input 
-        type="text" 
-        onChange={(e) => setRoomName(e.target.value)} 
-        placeholder="Room Name" 
-      />
-      <input 
-        type="text" 
-        onChange={(e) => setRoomInfo(e.target.value)} 
-        placeholder="Room Info" 
-      />
-      <button onClick={handleCreateRoom}>Create Room</button>
-      {roomUrl && <p>정상적으로 방이 생성되었습니다!</p>}
-      {/* {roomUrl && <p>Room URL: {roomUrl}</p>} */}
+      <div>
+        <input 
+          type="text" 
+          onChange={(e) => setRoomName(e.target.value)} 
+          placeholder="채팅방 이름" 
+        />
+        <input 
+          type="text" 
+          onChange={(e) => setRoomInfo(e.target.value)} 
+          placeholder="채팅방 설명" 
+        />
+      </div>
+      <button onClick={handleCreateRoom}>생성</button>
     </div>
   );
 };
